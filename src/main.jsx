cb246e132b5b282ed3d33daca833bb20afa7effc
checoluis2212 <luissergio.mkt@gmail.com>
2025-07-31 18:07:37 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter><App /></BrowserRouter>

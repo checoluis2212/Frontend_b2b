40c4063d3a748d735ba03a1defc5829eaf5a3793
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-13 16:59:38 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ResponsesList from './pages/ResponsesList';
-import ReclutamientoNative from './pages/ReclutamientoNative'; // <-- NUEVO
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/respuestas" element={<ResponsesList />} />
-      <Route path="/reclutamiento-native" element={<ReclutamientoNative />} /> {/* <-- NUEVO */}
     </Routes>
   );
 }

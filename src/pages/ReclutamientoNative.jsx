68b7787095383806f7819f9f8f092183a763247c
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-21 18:08:23 -0600

    Update ReclutamientoNative.jsx

--- src/pages/ReclutamientoNative.jsx
+++ src/pages/ReclutamientoNative.jsx
@@ -70,7 +70,7 @@ function ConfirmLeaveModal({ open, onClose, onConfirm }) {
             <img
               src="/rocket.gif"
               alt="Animación trabajo"
-              style={{ width: "70px", margin: "0 auto 16px auto", display: "block", marginBottom: 40px}}
+              className="rocket-gif"
              />
               
             <h2 id="modal-title" className="m-title">¿Quieres ir a buscar trabajo?</h2>
